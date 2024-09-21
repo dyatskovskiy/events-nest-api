515e8b931c6d81783653e7135bb32b573baddcc1
dyatskovskiy <d.yatskovskiy@ukr.net>
2024-09-21 23:20:54 +0300

    Added connection to the database

---
+++ src/modules/database/database.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { DatabaseProviders } from './database.provider';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
+  providers: [...DatabaseProviders],
+})
+export class DatabaseModule {}
