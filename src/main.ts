a5b76d8b3f0c79cdbdec41c72fc1b0171539b0c8
dyatskovskiy <d.yatskovskiy@ukr.net>
2024-09-21 21:58:06 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+import { HttpExceptionFilter } from './common/filters/HttpException.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const port = +process.env.PORT || 3000;
+
+  await app.listen(port);
+
+  console.log(`Application running on: ${await app.getUrl()}`);
+}
+bootstrap();
