153a8c344002cce8a411ff59e146c533dec32752
dyatskovskiy <d.yatskovskiy@ukr.net>
2024-09-22 19:09:30 +0300

    Added @types/mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './modules/database/database.module';
+import { EventsModule } from './modules/events/events.module';
+import { ParticipantsModule } from './modules/participants/participants.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, EventsModule, ParticipantsModule],
   controllers: [AppController],
   providers: [AppService],
 })
